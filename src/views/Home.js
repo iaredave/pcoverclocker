d1c21df2a355f8715ef2854404203dd31d20b7a8
iaredave <gouletdavid95@gmail.com>
2020-10-07 18:31:58 -0400

    Add Feature section - Added feature section beneath landing page intro

--- src/views/Home.js
+++ src/views/Home.js
@@ -56,7 +56,8 @@ import {
   UncontrolledCollapse,
   Nav,
   NavItem,
-  NavLink, Badge
+  NavLink,
+  Badge,
 } from "reactstrap"
 
 class Home extends React.Component {
@@ -109,11 +110,13 @@ class Home extends React.Component {
             <img
               alt="..."
               className="shapes wave"
+              style={{marginLeft: "80%"}}
               src={require("assets/img/waves.png")}
             />
             <img
               alt="..."
               className="shapes squares"
+              style={{margin: "50%"}}
               src={require("assets/img/patrat.png")}
             />
             <img
@@ -130,89 +133,100 @@ class Home extends React.Component {
               </Row>
               <Row>
                 <Col className="ml-auto mr-auto text-center" lg="6" md="7">
-                  {this.state.fading && <Button
-                    className="btn-simple btn-danger"
-                    color="default"
-                    href="#pablo"
-                    onClick={e => e.preventDefault()}
-                  >
-                    Level Up Today
-                  </Button>}
+                  {this.state.fading && (
+                    <Button
+                      className="btn-simple btn-danger"
+                      color="default"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Level Up Today
+                    </Button>
+                  )}
                 </Col>
               </Row>
             </Container>
           </div>
+          <div className="features-1">
+            <section className="section section-lg">
+              <img
+                alt="..."
+                className="path"
+                src={require("assets/img/path4.png")}
+              />
             <Container>
-              <div className="features-4">
-                <Container>
-                  <Row>
-                    <Col className="ml-auto mr-auto" md="8">
-                      <Badge color="primary" pill>
-                        Insight
-                      </Badge>
-                      <h1 className="title">Full-Funnel Social Analytics</h1>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="4">
-                      <div className="info info-hover">
-                        <div className="icon icon-primary">
-                          <img
-                            alt="..."
-                            className="bg-blob"
-                            src={require("assets/img/feature-blob/primary.png")}
-                          />
-                          <i className="tim-icons icon-user-run" />
-                        </div>
-                        <h4 className="info-title">Social Conversations</h4>
-                        <p className="description">
-                          Gain access to the demographics, psychographics, and
-                          location of unique people.
-                        </p>
-                      </div>
-                    </Col>
-                    <Col md="4">
-                      <div className="info info-hover">
-                        <div className="icon icon-success">
-                          <img
-                            alt="..."
-                            className="bg-blob"
-                            src={require("assets/img/feature-blob/success.png")}
-                          />
-                          <i className="tim-icons icon-atom" />
-                        </div>
-                        <h4 className="info-title">Analyze Performance</h4>
-                        <p className="description">
-                          Unify data from Facebook, Instagram, Twitter, LinkedIn,
-                          and Youtube to gain rich insights.
-                        </p>
-                      </div>
-                    </Col>
-                    <Col md="4">
-                      <div className="info info-hover">
-                        <div className="icon icon-warning">
-                          <img
-                            alt="..."
-                            className="bg-blob"
-                            src={require("assets/img/feature-blob/warning.png")}
-                          />
-                          <i className="tim-icons icon-gift-2" />
-                        </div>
-                        <h4 className="info-title">Measure Conversions</h4>
-                        <p className="description">
-                          Track actions taken on your website, understand the impact
-                          on your bottom line.
-                        </p>
-                      </div>
-                    </Col>
-                  </Row>
-                </Container>
-              </div>
+              <img
+                alt="..."
+                className="path"
+                src={require("assets/img/path4.png")}
+              />
+              <Row>
+                <Col className="ml-auto mr-auto" md="8">
+                  <Badge color="primary" pill>
+                    Insight
+                  </Badge>
+                  <h1 className="title">Full-System Overclock Tuning</h1>
+                </Col>
+              </Row>
+              <Row>
+                <Col md="4">
+                  <div className="info info-hover">
+                    <div className="icon icon-primary">
+                      <img
+                        alt="..."
+                        className="bg-blob"
+                        src={require("assets/img/feature-blob/primary.png")}
+                      />
+                      <i className="tim-icons icon-user-run" />
+                    </div>
+                    <h4 className="info-title">Social Conversations</h4>
+                    <p className="description">
+                      Gain access to the demographics, psychographics, and
+                      location of unique people.
+                    </p>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="info info-hover">
+                    <div className="icon icon-success">
+                      <img
+                        alt="..."
+                        className="bg-blob"
+                        src={require("assets/img/feature-blob/success.png")}
+                      />
+                      <i className="tim-icons icon-atom" />
+                    </div>
+                    <h4 className="info-title">Analyze Performance</h4>
+                    <p className="description">
+                      Unify data from Facebook, Instagram, Twitter, LinkedIn,
+                      and Youtube to gain rich insights.
+                    </p>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="info info-hover">
+                    <div className="icon icon-warning">
+                      <img
+                        alt="..."
+                        className="bg-blob"
+                        src={require("assets/img/feature-blob/warning.png")}
+                      />
+                      <i className="tim-icons icon-gift-2" />
+                    </div>
+                    <h4 className="info-title">Measure Conversions</h4>
+                    <p className="description">
+                      Track actions taken on your website, understand the impact
+                      on your bottom line.
+                    </p>
+                  </div>
+                </Col>
+              </Row>
             </Container>
+            </section>
           </div>
-
+        </div>
       </>
-    );
+    )
   }
 }
 
