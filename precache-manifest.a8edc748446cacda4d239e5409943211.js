6cc67f2f5cfae1e6eae0af3731698196337b5e8d
iaredave <gouletdavid95@gmail.com>
2020-10-12 14:34:42 -0400

    Updates

---
+++ precache-manifest.a8edc748446cacda4d239e5409943211.js
@@ -0,0 +1,402 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76220bffd455fbfc48a32a52a641c4ae",
+    "url": "/pcoverclocker/index.html"
+  },
+  {
+    "revision": "cf5d264557c37f397200",
+    "url": "/pcoverclocker/static/css/main.3cab7d15.chunk.css"
+  },
+  {
+    "revision": "2fb3d940051d33476a37",
+    "url": "/pcoverclocker/static/js/2.30a2ac67.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/pcoverclocker/static/js/2.30a2ac67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5d264557c37f397200",
+    "url": "/pcoverclocker/static/js/main.49dbc7c5.chunk.js"
+  },
+  {
+    "revision": "2b9e3c85dc8dae642812",
+    "url": "/pcoverclocker/static/js/runtime-main.b40855af.js"
+  },
+  {
+    "revision": "0bf2cfe5af714af67531c4409cf81568",
+    "url": "/pcoverclocker/static/media/1234.0bf2cfe5.png"
+  },
+  {
+    "revision": "a9d5193776a181a58fef670a803de5b3",
+    "url": "/pcoverclocker/static/media/404-error.a9d51937.png"
+  },
+  {
+    "revision": "26ac0aba696a4e3c906d6cabe6c01341",
+    "url": "/pcoverclocker/static/media/500-error.26ac0aba.png"
+  },
+  {
+    "revision": "61e7309f8fcfda5335038432e84578b3",
+    "url": "/pcoverclocker/static/media/about-us.61e7309f.png"
+  },
+  {
+    "revision": "8cd746690f52967e8aaf8a9b2ebb8aa9",
+    "url": "/pcoverclocker/static/media/accordion.8cd74669.png"
+  },
+  {
+    "revision": "e6d8421ea94a8e72febcb55df8788892",
+    "url": "/pcoverclocker/static/media/account-settings.e6d8421e.png"
+  },
+  {
+    "revision": "a673e080f4ba816b1cca4d954013f2b2",
+    "url": "/pcoverclocker/static/media/blob.a673e080.png"
+  },
+  {
+    "revision": "7eb53e9d4e598ed0a67b05d37f54bb63",
+    "url": "/pcoverclocker/static/media/blog-post.7eb53e9d.png"
+  },
+  {
+    "revision": "8fdb26333fb3c3c0e67d2064b41f5897",
+    "url": "/pcoverclocker/static/media/blog-posts.8fdb2633.png"
+  },
+  {
+    "revision": "edeecc4cc033ebfdf8cea22f3fb39199",
+    "url": "/pcoverclocker/static/media/blogpost1.edeecc4c.png"
+  },
+  {
+    "revision": "93be3e84c03f30e5df916828db8619d1",
+    "url": "/pcoverclocker/static/media/blogpost2.93be3e84.png"
+  },
+  {
+    "revision": "1a3c116bfbe27a7ee181e02638225962",
+    "url": "/pcoverclocker/static/media/blogpost3.1a3c116b.png"
+  },
+  {
+    "revision": "308cab87ce75f00638e269104e5baa38",
+    "url": "/pcoverclocker/static/media/blogpost4.308cab87.png"
+  },
+  {
+    "revision": "6c04a1918a2293b5bb67136e8c8700a4",
+    "url": "/pcoverclocker/static/media/blogpost5.6c04a191.png"
+  },
+  {
+    "revision": "9d90c1ae8fd31c7085dba19f9ea079c1",
+    "url": "/pcoverclocker/static/media/blogpost6.9d90c1ae.png"
+  },
+  {
+    "revision": "95cd0b2307d3f7f3a647484efcf67cc0",
+    "url": "/pcoverclocker/static/media/blogpost7.95cd0b23.png"
+  },
+  {
+    "revision": "62034e05de8f58a811b78150d07a9ac9",
+    "url": "/pcoverclocker/static/media/card-btn.62034e05.png"
+  },
+  {
+    "revision": "8384a59386f2d887b9453755533d1cc8",
+    "url": "/pcoverclocker/static/media/card-orange.8384a593.png"
+  },
+  {
+    "revision": "cba4218ccb321a01d653658f177cd868",
+    "url": "/pcoverclocker/static/media/cercuri.cba4218c.png"
+  },
+  {
+    "revision": "840053f2af5f2564ddc2c75e51373cd4",
+    "url": "/pcoverclocker/static/media/chat-page.840053f2.png"
+  },
+  {
+    "revision": "f23e9f8be931c489384b6934f57f51ee",
+    "url": "/pcoverclocker/static/media/checkout-page.f23e9f8b.png"
+  },
+  {
+    "revision": "e07d11ab1425a81b8b08c53d3e06e177",
+    "url": "/pcoverclocker/static/media/contact.e07d11ab.png"
+  },
+  {
+    "revision": "443133774189ab7986aee631cd93538a",
+    "url": "/pcoverclocker/static/media/contact1.44313377.png"
+  },
+  {
+    "revision": "8eda4f51e64d587cb709f00627229c98",
+    "url": "/pcoverclocker/static/media/contact2.8eda4f51.png"
+  },
+  {
+    "revision": "600d3649a73a3c8e8b812a8b1ce2ef42",
+    "url": "/pcoverclocker/static/media/contact3.600d3649.png"
+  },
+  {
+    "revision": "93c1f9d5e697a7220e61de5cd6444d88",
+    "url": "/pcoverclocker/static/media/contact4.93c1f9d5.png"
+  },
+  {
+    "revision": "2a6299e680e89598b5fe6e6738990204",
+    "url": "/pcoverclocker/static/media/content.2a6299e6.png"
+  },
+  {
+    "revision": "e71827e6fc2e54f5aecdf887d8a1b02c",
+    "url": "/pcoverclocker/static/media/content1.e71827e6.png"
+  },
+  {
+    "revision": "e0b237e8b205133d689761dfe922e6cd",
+    "url": "/pcoverclocker/static/media/dots.e0b237e8.png"
+  },
+  {
+    "revision": "a22ade45d7cd28cf0eaecc99552a5455",
+    "url": "/pcoverclocker/static/media/dribbble-btn.a22ade45.png"
+  },
+  {
+    "revision": "37b2dff9a730fa3b3a2a4f6acf94a0da",
+    "url": "/pcoverclocker/static/media/ecommerce.37b2dff9.png"
+  },
+  {
+    "revision": "c624379758023f9333d640528fa6d76e",
+    "url": "/pcoverclocker/static/media/elements-bg.c6243797.png"
+  },
+  {
+    "revision": "271f2d4a028e38b1c169fa724b739811",
+    "url": "/pcoverclocker/static/media/features1.271f2d4a.png"
+  },
+  {
+    "revision": "ce86a5b766c5c54ebff48aa1c6624760",
+    "url": "/pcoverclocker/static/media/features2.ce86a5b7.png"
+  },
+  {
+    "revision": "ae2eb80ed100406d65e095d089887dab",
+    "url": "/pcoverclocker/static/media/features3.ae2eb80e.png"
+  },
+  {
+    "revision": "9347b84a5f832a5e5c8eb5de9b7b1163",
+    "url": "/pcoverclocker/static/media/features4.9347b84a.png"
+  },
+  {
+    "revision": "4959e0d64fcd9144ddb0cd778483ef69",
+    "url": "/pcoverclocker/static/media/features5.4959e0d6.png"
+  },
+  {
+    "revision": "a16ffc50c2d76ea761e1b6b0004216b8",
+    "url": "/pcoverclocker/static/media/features6.a16ffc50.png"
+  },
+  {
+    "revision": "93895aae8aa48b5f9bb41ba214e77023",
+    "url": "/pcoverclocker/static/media/header1.93895aae.png"
+  },
+  {
+    "revision": "c06964ec557b8991f197b92ae6088664",
+    "url": "/pcoverclocker/static/media/header2.c06964ec.png"
+  },
+  {
+    "revision": "74def01def5c984888f0b7a156182f86",
+    "url": "/pcoverclocker/static/media/header3.74def01d.png"
+  },
+  {
+    "revision": "1c2fe1f7e14f717c7490781a4e244d93",
+    "url": "/pcoverclocker/static/media/header4.1c2fe1f7.png"
+  },
+  {
+    "revision": "a7f1575531c37b84e3cabd37a51a3d2a",
+    "url": "/pcoverclocker/static/media/header5.a7f15755.png"
+  },
+  {
+    "revision": "c439bb2a57899676d8b18b4b74cb74e9",
+    "url": "/pcoverclocker/static/media/header6.c439bb2a.png"
+  },
+  {
+    "revision": "c9f4d74a66ce95d3be63f4a0dadaff25",
+    "url": "/pcoverclocker/static/media/invoice-page.c9f4d74a.png"
+  },
+  {
+    "revision": "1d8087893a676190beb62d738027aa75",
+    "url": "/pcoverclocker/static/media/ipad3.1d808789.png"
+  },
+  {
+    "revision": "b426db5716d709d3f7943b084d9ba79b",
+    "url": "/pcoverclocker/static/media/johana.b426db57.jpg"
+  },
+  {
+    "revision": "6fb21e5f10176e93a21df44b6e72991c",
+    "url": "/pcoverclocker/static/media/landing.6fb21e5f.png"
+  },
+  {
+    "revision": "e4e5ffb5ee7717da0293c2ba27d284d7",
+    "url": "/pcoverclocker/static/media/login.e4e5ffb5.png"
+  },
+  {
+    "revision": "0b276fd5496499ade4c42321f61891b7",
+    "url": "/pcoverclocker/static/media/michael.0b276fd5.jpg"
+  },
+  {
+    "revision": "eaf444f6c21d8fa4eaf9b1a47be2b504",
+    "url": "/pcoverclocker/static/media/noah-wetering.eaf444f6.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/pcoverclocker/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/pcoverclocker/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/pcoverclocker/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/pcoverclocker/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "28d1175d426ba5297c6d42f8f0c35277",
+    "url": "/pcoverclocker/static/media/p10.28d1175d.jpg"
+  },
+  {
+    "revision": "d49abcffd647ad8e2cf1550713762f7e",
+    "url": "/pcoverclocker/static/media/path2.d49abcff.png"
+  },
+  {
+    "revision": "e62d30e8e78373f1b42b50aae990f63c",
+    "url": "/pcoverclocker/static/media/path3.e62d30e8.png"
+  },
+  {
+    "revision": "43994fd667e8c2dae21565d369e53322",
+    "url": "/pcoverclocker/static/media/path4.43994fd6.png"
+  },
+  {
+    "revision": "3f151d8bef8496e30b1e0a89ea606862",
+    "url": "/pcoverclocker/static/media/pricing.3f151d8b.png"
+  },
+  {
+    "revision": "3a772364fe33916e215e11e3c923f0a6",
+    "url": "/pcoverclocker/static/media/pricing1.3a772364.png"
+  },
+  {
+    "revision": "0d6b40d2442d112c775efde034f84f7e",
+    "url": "/pcoverclocker/static/media/pricing2.0d6b40d2.png"
+  },
+  {
+    "revision": "d80900b3506e0e68ebefd76958933f54",
+    "url": "/pcoverclocker/static/media/pricing3.d80900b3.png"
+  },
+  {
+    "revision": "d21d6e99bb49d4daebbd81eb27bc85f6",
+    "url": "/pcoverclocker/static/media/pricing4.d21d6e99.png"
+  },
+  {
+    "revision": "1c679252f02f07075b2703646e26fc8d",
+    "url": "/pcoverclocker/static/media/pricing5.1c679252.png"
+  },
+  {
+    "revision": "5750fb1951424c67b3b1a874a20a484b",
+    "url": "/pcoverclocker/static/media/pricing6.5750fb19.png"
+  },
+  {
+    "revision": "eaaceade7b3401be55529d606fc81ea6",
+    "url": "/pcoverclocker/static/media/product.eaaceade.png"
+  },
+  {
+    "revision": "15dc07aef84c7c5f971ea21beec341b5",
+    "url": "/pcoverclocker/static/media/profile.15dc07ae.png"
+  },
+  {
+    "revision": "26f22b04fd2e7a5c6cc162f7258029fc",
+    "url": "/pcoverclocker/static/media/projects1.26f22b04.png"
+  },
+  {
+    "revision": "427b2887e236073e728af784b36d0359",
+    "url": "/pcoverclocker/static/media/projects2.427b2887.png"
+  },
+  {
+    "revision": "ddb04bb54a2017ac15ffac1a01a5cfb1",
+    "url": "/pcoverclocker/static/media/projects3.ddb04bb5.png"
+  },
+  {
+    "revision": "c58baa153e39b250f2d6a478ac0bcc61",
+    "url": "/pcoverclocker/static/media/projects4.c58baa15.png"
+  },
+  {
+    "revision": "98f1388292e4c69c6d84bde447c07e3b",
+    "url": "/pcoverclocker/static/media/randy-colas.98f13882.jpg"
+  },
+  {
+    "revision": "1ff7e754a79f7494720e12c190d238a7",
+    "url": "/pcoverclocker/static/media/register.1ff7e754.png"
+  },
+  {
+    "revision": "8ba51f97d2b7a0f19bfbb0b8ffbb05f8",
+    "url": "/pcoverclocker/static/media/reset-page.8ba51f97.png"
+  },
+  {
+    "revision": "4c450c4650317028b56c748085d07146",
+    "url": "/pcoverclocker/static/media/ryan.4c450c46.png"
+  },
+  {
+    "revision": "36484533858fca5e6187dd33f8bc5810",
+    "url": "/pcoverclocker/static/media/sendra-martorell.36484533.jpg"
+  },
+  {
+    "revision": "5d370eaf3004a832d83b2294c28f4165",
+    "url": "/pcoverclocker/static/media/square1.5d370eaf.png"
+  },
+  {
+    "revision": "5b1e54e0390a39ac23e39138e5702682",
+    "url": "/pcoverclocker/static/media/square2.5b1e54e0.png"
+  },
+  {
+    "revision": "ef4e0a705ca9b44a413e24cc2d4f4a41",
+    "url": "/pcoverclocker/static/media/square3.ef4e0a70.png"
+  },
+  {
+    "revision": "eb5c914326e0b14b7b9d0994124d6eca",
+    "url": "/pcoverclocker/static/media/square4.eb5c9143.png"
+  },
+  {
+    "revision": "fc998805d72848ccda24724342bb32f6",
+    "url": "/pcoverclocker/static/media/square5.fc998805.png"
+  },
+  {
+    "revision": "aacfb4e2ba8b8bd2c85e473656e9ce0b",
+    "url": "/pcoverclocker/static/media/table.aacfb4e2.png"
+  },
+  {
+    "revision": "59397986264ccf83fe1b07aee57f4b81",
+    "url": "/pcoverclocker/static/media/table1.59397986.png"
+  },
+  {
+    "revision": "9fbf0e2972f2569ef2ce92813dba3ca6",
+    "url": "/pcoverclocker/static/media/table2.9fbf0e29.png"
+  },
+  {
+    "revision": "9415639ea8bbb3fb439294849819c32b",
+    "url": "/pcoverclocker/static/media/team1.9415639e.png"
+  },
+  {
+    "revision": "b1e4f4976ee80122e2cf3ba510d70084",
+    "url": "/pcoverclocker/static/media/team2.b1e4f497.png"
+  },
+  {
+    "revision": "0c53d5b80fcaf0da086e8006a59e5b3f",
+    "url": "/pcoverclocker/static/media/team3.0c53d5b8.png"
+  },
+  {
+    "revision": "e779dead4bee62846518cbd1b04457ef",
+    "url": "/pcoverclocker/static/media/team4.e779dead.png"
+  },
+  {
+    "revision": "37547207b8b470c4f33b3ea7216927f1",
+    "url": "/pcoverclocker/static/media/team5.37547207.png"
+  },
+  {
+    "revision": "77f04b3359aac3af58b134573b810620",
+    "url": "/pcoverclocker/static/media/testimonials1.77f04b33.png"
+  },
+  {
+    "revision": "9406c47f8a432dd250723cf1b6bd6d88",
+    "url": "/pcoverclocker/static/media/testimonials3.9406c47f.png"
+  },
+  {
+    "revision": "dae764df8caaa6a85a36eedc239202d8",
+    "url": "/pcoverclocker/static/media/testimonials4.dae764df.png"
+  },
+  {
+    "revision": "edff6a9bd5ebc2be8f9cfe23067eaa3a",
+    "url": "/pcoverclocker/static/media/tom-klein.edff6a9b.jpg"
+  }
+]);
